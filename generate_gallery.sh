6adc0c1706d062329ee053874f52613f5da079cd
richbl <richbl@gmail.com>
2018-01-07 22:04:18 -0800

    Update script against shellcheck linter

--- generate_gallery.sh
+++ generate_gallery.sh
@@ -18,7 +18,6 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 #
 # A bash script to prep (rename, resize, reduce, and watermark) a folder of
 # images, and then generate an index in HTML
-# version: 0.4.0
 #
 # requirements:
 #  --A folder full of image files
@@ -39,10 +38,10 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 #
 shopt -s extglob
 EXEC_DIR="$(dirname "$0")"
-. ${EXEC_DIR}/lib/args
-. ${EXEC_DIR}/lib/general
-
-ARGS_FILE="${EXEC_DIR}/data/config.json"
+# shellcheck source=lib/args
+source "${EXEC_DIR}/lib/args"
+# shellcheck source=lib/general
+source "${EXEC_DIR}/lib/general"
 
 declare -a REQ_PROGRAMS=('jq' 'convert')
 declare -a REQ_FILES='('"${EXEC_DIR}"'/dbi_watermark.png)'
@@ -50,8 +49,8 @@ declare -a REQ_FILES='('"${EXEC_DIR}"'/dbi_watermark.png)'
 # -----------------------------------------------------------------------------
 # perform script configuration, arguments parsing, and validation
 #
-check_program_dependencies "REQ_PROGRAMS[@]"
-check_file_dependencies "REQ_FILES[@]"
+check_program_dependencies "${REQ_PROGRAMS[@]}"
+check_file_dependencies "${REQ_FILES[@]}"
 display_banner
 scan_for_args "$@"
 check_for_args_completeness
@@ -59,22 +58,22 @@ check_for_args_completeness
 # -----------------------------------------------------------------------------
 # copy files to output_dir
 #
-echo "Copying source files to destination..."
+printf "%s" "Copying source files to destination... "
 
 mkdir -p "$(get_config_arg_value output_dir)"
 cp "$(get_config_arg_value input_dir)"/* "$(get_config_arg_value output_dir)"
 
-echo "Copying source files to destination complete."
-echo
+printf "%s\n\n" "complete."
 
 # -----------------------------------------------------------------------------
 # transpose filenames (lowercase and legal posix name)
 #
-echo "Transposing filesnames..."
+printf "%s" "Transposing filesnames... "
 
 # get directory names
-LC_DIRNAME=`echo $(get_config_arg_value output_dir) | sed 's/.*/\L&/g'`
-POSIX_DIRNAME=`echo ${LC_DIRNAME} | sed 's/[^A-Za-z0-9\-\._\/]/_/g'`
+RAW_DIRNAME="$(get_config_arg_value output_dir)"
+LC_DIRNAME="${RAW_DIRNAME//.*/\L&/g}"
+POSIX_DIRNAME="${LC_DIRNAME//[^A-Za-z0-9\-\._\/]/_/g}"
 
 # set filenames to lowercase
 find "$(get_config_arg_value output_dir)" -depth -name "*" -exec basename "{}" \; -execdir rename 'y/A-Z/a-z/' "{}" \; &>/dev/null
@@ -85,47 +84,42 @@ find "${LC_DIRNAME}" -depth -name "*" -exec basename "{}" \; -execdir rename 's/
 # set tmp filename extension
 find "${LC_DIRNAME}" -type f -exec mv '{}' '{}'.tmp \; &>/dev/null
 
-echo "Transposing filesnames complete."
-echo
+printf "%s\n\n" "complete."
 
 # -----------------------------------------------------------------------------
 # reducing file sizes
 #
-echo "Reducing file sizes and applying watermark..."
+printf "%s" "Reducing file sizes and applying watermark... "
 
 for file in "${POSIX_DIRNAME}"/*.tmp; do
-  convert $file -quality 50 -auto-orient -resize x1000 ${EXEC_DIR}/dbi_watermark.png -gravity southeast -geometry +15+15 -composite "${POSIX_DIRNAME}"/reduced\_$(basename ${file%.*})
-  rm $file
+  convert "$file" -quality 50 -auto-orient -resize x1000 "${EXEC_DIR}/dbi_watermark.png" -gravity southeast -geometry +15+15 -composite "${POSIX_DIRNAME}"/reduced_"$(basename "${file%.*}")"
+  rm "$file"
 done
 
-echo "Reducing file sizes and applying watermark complete."
-echo
+printf "%s\n\n" "complete."
 
 # -----------------------------------------------------------------------------
 # generate files list (HTML)
 #
-echo "Creating HTML output file..."
+printf "%s" "Creating HTML output file... "
 
 # generate results file (sorted)
 TMPFILE=$(mktemp);
 
-for i in $(find "${POSIX_DIRNAME}" -type f -name '*' -exec basename "{}" \;); do
-  echo '<div class="file" data-type="image" data-source="./images/work/'$(basename ${POSIX_DIRNAME})'/'${i}'" data-caption="" data-album="'$(get_config_arg_value gallery)'"></div>' >> ${TMPFILE}
+find "${POSIX_DIRNAME}" -type f -name '*' -exec basename "{}" \; | while read -r fname; do
+  echo '<div class="file" data-type="image" data-source="./images/work/'"$(basename "${POSIX_DIRNAME}")"'/'"${fname}"'" data-caption="" data-album="'"$(get_config_arg_value gallery)"'"></div>' >> "${TMPFILE}"
 done
 
 # sort content
-sort ${TMPFILE} -o "$(get_config_arg_value output_dir)".results
+sort "${TMPFILE}" -o "$(get_config_arg_value output_dir)".results
 
 # generate results file
-LINE="<li data-album=$(get_config_arg_value gallery)>$(get_config_arg_value gallery)</li>
+LINE="<li data-album=\"$(get_config_arg_value gallery)\">$(get_config_arg_value gallery)</li>
 
 <!-- BEGIN $(get_config_arg_value gallery) gallery images -->"
 
-echo "${LINE}" | cat - "$(get_config_arg_value output_dir)".results > ${TMPFILE} && mv ${TMPFILE} "$(get_config_arg_value output_dir)".results
-echo '<!-- END '$(get_config_arg_value gallery)' gallery images -->'>> "$(get_config_arg_value output_dir)".results
+echo "${LINE}" | cat - "$(get_config_arg_value output_dir)".results > "${TMPFILE}" && mv "${TMPFILE}" "$(get_config_arg_value output_dir)".results
+echo '<!-- END '"$(get_config_arg_value gallery)"' gallery images -->'>> "$(get_config_arg_value output_dir)".results
 
-echo "Creating HTML output file complete."
-
-echo
-echo "Success. Results file ($(basename ${POSIX_DIRNAME}).results) created in $(get_config_arg_value output_dir)."
-echo
+printf "%s\n\n" "complete."
+printf "%s\n\n" "Success. Results file ($(basename "${POSIX_DIRNAME}").results) created in $(get_config_arg_value output_dir)."
